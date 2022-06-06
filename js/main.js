8bd90cadb267d676c1b985e5d96262d3c5fbfdf0
MintyFreshBro <102497017+MintyFreshBro@users.noreply.github.com>
2022-06-05 23:50:07 -0400

    updating html and main js files

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 alert('Working')
+
+document.querySelector('button').addEventListener('click', getCity)
+
+function getCity(){
+    // fetch(`server.js`)
+    // .then(response => response.json())
+    // .then(data => {
+    //     document.querySelector('.city').innerText = data.city
+    // })
+}
