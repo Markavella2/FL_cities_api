2cfebfb543855e5a47b6c8fbe80500d046515d5b
MintyFreshBro <102497017+MintyFreshBro@users.noreply.github.com>
2022-06-05 22:33:55 -0400

    updating files for FL cities attractions

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
+//const cors = require('cors')
 const PORT = 8000
 
-app.use(cors())
+//app.use(cors())
 
-let cities = {
+const cities = {
     'orlando': {
         'city': 'Orlando',
         'mainAttraction': 'Walt Disney World & Universal Studios',
@@ -85,8 +85,6 @@ let cities = {
         'Population': 'I would guess between 1 and 50,000,000',
     }
 }
-
-//Look at and fix below!
 
 
 app.get('/', (request, response) => {
